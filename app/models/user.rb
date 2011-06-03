9946eb9654997a6223f6a0c6ceb99b07b40a193a
Percy Keeley <percy.keeley@softwareForest.com>
2011-06-03 12:48:57 -0400

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,7 +74,12 @@ class User < ActiveRecord::Base
     user && user.has_password?(submitted_password) ? user : nil
   end
 
-
+#Listing 9.17. Adding an authenticate_with_salt method to the User model.
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
+ 
   private
 
     def encrypt_password
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -26,33 +26,58 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
   end
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> modeling-users
   it "should have the right links on the layout" do
     visit root_path
+
     click_link "About"
     response.should have_selector('title', :content => "About")
+
     click_link "Help"
-<<<<<<< HEAD
-    response.should # fill in
-    click_link "Contact"
-    response.should # fill in
-    click_link "Home"
-    response.should # fill in
-    click_link "Sign up now!"
-    response.should # fill in
-=======
     response.should have_selector('title', :content => "Help")
+
     click_link "Contact"
     response.should have_selector('title', :content => "Contact")
+
     click_link "Home"
     response.should have_selector('title', :content => "Home")
+
     click_link "Sign up now!"
     response.should  have_selector('title', :content => "Sign up")
->>>>>>> modeling-users
+
+  end
+
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit root_path
+      response.should have_selector("a", :href => signin_path,
+                                         :content => "Sign in")
+    end
+  end
+
+  describe "when signed in" do
+
+    before(:each) do
+      @user = Factory(:user)
+#      visit signin_path
+#      fill_in :email,    :with => @user.email
+#      fill_in :password, :with => @user.password
+#      click_button
+       integration_sign_in(@user)
+    end
+
+    it "should have a signout link" do
+      visit root_path
+      response.should have_selector("a", :href => signout_path,
+                                         :content => "Sign out")
+    end
+
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+                                         :content => "Profile")
+    end
+    
   end
 
 end
